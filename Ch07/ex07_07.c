cc811650efe2adbdf9c0cef98ed730529ac92951
suyeon <130628673+suyeon0121@users.noreply.github.com>
2023-08-18 12:15:11 +0900

    Add files via upload

---
+++ Ch07/ex07_07.c
@@ -0,0 +1,19 @@
+﻿#pragma warning(disable : 4996)
+#include <stdio.h>
+
+int main(void)
+{
+	int dan, cnt, gob;
+
+	for (dan = 2; dan <= 9; dan++)
+	{
+		for (cnt = 1; cnt <= 9; cnt++)
+		{
+			gob = dan * cnt;
+			printf("%d x %d = %d", dan, cnt, gob);
+			printf(" : 바깥쪽 for 문 %d일 때, 안쪽 for 문은 %d\n", dan, cnt);
+		}
+		printf("\n");
+	}
+	return 0;
+}
