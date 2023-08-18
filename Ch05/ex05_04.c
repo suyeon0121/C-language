3d75156c7562baf1338e6b964d18d39168a0b187
suyeon <130628673+suyeon0121@users.noreply.github.com>
2023-08-18 11:20:43 +0900

    Add files via upload

---
+++ Ch05/ex05_04.c
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int val, a;
+
+	a = 8;
+	val = ++a;
+
+	printf(" a = 8일 때 \n");
+	printf("1.val = ++a이면 val = %d, a = %d\n", val, a);
+
+	a = 8;
+	val = a++;
+	printf("2.val = a++이면 val = %d, a = %d\n", val, a);
+
+	a = 8;
+	val = --a;
+	printf("3.val = --a이면 val = %d, a = %d\n", val, a);
+
+	a = 8;
+	val = a--;
+	printf("4.val = a--이면 val = %d, a = %d\n", val, a);
+	return 0;
+}
