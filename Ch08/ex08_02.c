c44e10251f76bcbbd10aa54c978c3dba53c9d1d2
suyeon <130628673+suyeon0121@users.noreply.github.com>
2023-08-20 13:30:32 +0900

    Add files via upload

---
+++ Ch08/ex08_02.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define NUM 45
+
+int main(void)
+{
+	int cnt;
+
+	printf("1~45 중에서 숫자 6개 출력\n");
+	srand((unsigned)time(NULL));
+
+	printf("srand( ) 출력 : ");
+	for (cnt = 1; cnt <= 6; cnt++)
+		printf(" %d ", 1 + (rand() % NUM));
+
+	printf("\n");
+	return 0;
+}
